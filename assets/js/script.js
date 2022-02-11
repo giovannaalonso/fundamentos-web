87827ea296265377733a9463d884a953f6508ac3
Giovanna <giovannaalonso28@gmail.com>
2022-02-11 20:36:03 -0300

    mais autenticações com JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,19 @@ por setor: querySelector()
 */
 
 let nome = window.document.getElementById('nome')
-let email = document.querySelector('#e-mail')
-let email = document.querySelector('#assunto')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '80%'
+email.style.width = '80%'
 
 function validaNome(){
     let txt = document.querySelector('#txtNome')
     if (nome.value.length <3 ){
         txt.innerHTML = 'Nome Inválido!'
+        txt.style.color = "red"
         }else{
             txt.innerHTML = 'Nome Válido!'
+            txt.style.color = "green"
         }
 }

a371a429e7452e0c46b984741a2b5b6a5b01a1b1
Giovanna <giovannaalonso28@gmail.com>
2022-02-13 19:42:24 -0300

    melhorias de autenticações

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,56 @@ por setor: querySelector()
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk =  false
+let emailOk = false
+let assuntoOk =  false
 
 nome.style.width = '80%'
 email.style.width = '80%'
 
 function validaNome(){
-    let txt = document.querySelector('#txtNome')
+    let txtNome = document.querySelector('#txtNome')
     if (nome.value.length <3 ){
-        txt.innerHTML = 'Nome Inválido!'
-        txt.style.color = "red"
+        txtNome.innerHTML = 'Nome Inválido!'
+        txtNome.style.color = "red"
+       
         }else{
-            txt.innerHTML = 'Nome Válido!'
-            txt.style.color = "green"
+            txtNome.innerHTML = 'Nome Válido!'
+            txtNome.style.color = "green"
+            nomeOk =  true
         }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido!'
+        txtEmail.style.color = 'red'
+        
+    }else{
+            txtEmail.innerHTML = 'E-mail Válido!'
+            txtEmail.style.color = "green"
+            emailOk = true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length <= 100){
+        txtAssunto.innerHTML = 'Assunto válido.'
+        txtAssunto.style.color = 'green'
+        assuntoOk =  true
+        
+    }else{
+        txtAssunto.innerHTML = 'Número de caracteres máximo atingido.'
+        txtAssunto.style.color = "red"
+    }
+}
+function enviar() {
+    //console.log("entrei")
+    if(nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
+    }else{
+        alert('Preencha o formulário corretamente antes de enviar')
+    }
 }
